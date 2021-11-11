968d8e4e972d45fa54ad40c4a352d2b3e2b8f0c1
toster11100 <victorchuguev@gmail.com>
2021-11-11 22:33:50 +0300

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssedgeki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 21:48:47 by ssedgeki          #+#    #+#             */
+/*   Updated: 2021/11/11 21:48:49 by ssedgeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	res = (char *)malloc(ft_strlen(s1) + (ft_strlen(s2) + 1));
+	if (!res)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	return (res);
+}
