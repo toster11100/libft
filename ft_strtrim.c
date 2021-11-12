bee28394c23547ce029236b497ca8069cb3f035b
toster11100 <victorchuguev@gmail.com>
2021-11-12 12:33:59 +0300

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssedgeki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 22:49:29 by ssedgeki          #+#    #+#             */
+/*   Updated: 2021/11/11 22:49:31 by ssedgeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	s_len;
+	char	*res;
+
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	s_len = ft_strlen(s1);
+	while (s_len && ft_strchr(set, s1[s_len]))
+		s_len--;
+	res = ft_substr(s1, 0, s_len + 1);
+	return (res);
+}
--- libft.h
+++ libft.h
@@ -40,4 +40,5 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 #endif
