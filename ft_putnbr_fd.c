80f8973a832a732b999e037bc19855cf073cd42d
toster11100 <victorchuguev@gmail.com>
2021-11-12 22:08:18 +0300

    fix

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,27 +14,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
-
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
 	if (n < 0)
 	{
-		write(fd, "-", 1);
-		if (n == -2147483648)
-		{
-			write(fd, "2147483648", 10);
-			return ;
-		}
+		ft_putchar_fd('-', fd);
 		n = -n;
 	}
-	if (n < 10)
-	{
-		c = (n % 10) + '0';
-		ft_putchar_fd(c, fd);
-	}
-	else
+	if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		c = (n % 10) + '0';
-		ft_putchar_fd(c, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
